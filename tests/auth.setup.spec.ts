8aa13f31de39043df238d04f1c91e971d5a802b9
Vlad <dynamofan27@gmail.com>
2025-09-10 00:03:15 +0300

    Fix Unit-14

--- tests/auth.setup.spec.ts
+++ tests/auth.setup.spec.ts
@@ -1,26 +1,14 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
 import path from 'path';
 
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
-let token: string;
+test('login with valid credential', async ({ page }) => {
+    const loginPage = new LoginPage(page);
 
-test('login with valid credential', async ({ page, request }) => {
-    const resp = await request.post('https://api.practicesoftwaretesting.com', {
-        data: { 
-            'email': 'customer@practicesoftwaretesting.com', 
-            'password': 'welcome01',
-        }
-    });
-
-    const jsonData = await resp.json(); 
-    token = jsonData.access_token;
-
-    await page.goto('');
-    await page.evaluate((token) => {
-        localStorage.setItem('auth-token', token);
-    }, token);
-    
-    await page.reload();
+    await loginPage.goto();
+    await loginPage.login('customer@practicesoftwaretesting.com', 'welcome01');
+    await expect(page).toHaveURL(/\/account$/);
     await page.context().storageState({ path: authFile });
 });
